e29ec77f99e6a252dbca7645d79abd4231358005
yutong pang <wstcpyt@gmail.com>
2015-07-10 09:01:09 -0400

    remove fftw and fftwcpp module

--- cmake/Modules/fftw.cmake
+++
@@ -1,4 +0,0 @@
-set(FFTW_DIR "../../cpplib/fftw-3.3.4"
-    CACHE PATH "The path to the FFTW_DIR framework.")
-add_subdirectory(${FFTW_DIR} ${CMAKE_BINARY_DIR}/fftw)
-include_directories(SYSTEM ${FFTW_DIR}/api)
--- cmake/Modules/fftwpp.cmake
+++
@@ -1,4 +0,0 @@
-set(FFTWPP_DIR "../../cpplib/fftw++-1.13"
-    CACHE PATH "The path to the FFTWPP_DIR framework.")
-add_subdirectory(${FFTWPP_DIR} ${CMAKE_BINARY_DIR}/fftwpp)
-include_directories(SYSTEM ${FFTWPP_DIR})
